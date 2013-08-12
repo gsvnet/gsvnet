85d5fe0b5638eda123a54808b97cbc47d193cf4d
Mark Redeman <MarkRedeman@gmail.com>
2013-08-12 17:35:23 +0200

    Standaard layout toegeveogd en index view aangemaakt

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -11,7 +11,12 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', [
+        'as' => 'home',
+        'uses' => 'HomeController@showIndex'
+    ]
+);
+// Route::get('/', function()
+// {
+// 	return View::make('hello');
+// });
