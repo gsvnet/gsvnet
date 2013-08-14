ef5cb68cc6dd2cbf4378019760dcfb215a2cc323
Mark Redeman <MarkRedeman@gmail.com>
2013-08-14 22:34:11 +0200

    Migrations voor users, members en events aangemaakt

---
+++ laravel/app/database/migrations/2013_08_14_201808_create_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateEventsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('events', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('title');
+			$table->text('description');
+			$table->datetime('start_date');
+			$table->datetime('end_date')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('events');
+	}
+
+}
