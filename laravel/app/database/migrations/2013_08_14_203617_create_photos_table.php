2c87d471e3f2c12d64847e2704c8db7de51a1a96
Mark Redeman <MarkRedeman@gmail.com>
2013-08-14 22:38:00 +0200

    Migrations voor fotos en albums gemaakt

---
+++ laravel/app/database/migrations/2013_08_14_203617_create_photos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePhotosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photos', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->integer('album_id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('photos');
+	}
+
+}
