208fa4d3b8018b5eff781a9aa9731ea08f6a6c67
Mark Redeman <MarkRedeman@gmail.com>
2013-08-15 20:55:44 +0200

    Login code in aparte view gezet

--- laravel/app/views/layouts/default.blade.php
+++ laravel/app/views/layouts/default.blade.php
@@ -41,28 +41,7 @@
         <p>Caput sapientiae est reverentia domini</p>
     </footer>
 
-    <div id="login-dialog" class="zoom-anim-dialog mfp-hide">
-        <h2>Login</h2>
-        <form>
-
-            <div class="form-group">
-                <label class="control-label" for="inputEmail">Email</label>
-                <input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Email">
-            </div>
-            <div class="form-group">
-                <label class="control-label" for="inputPassword">Wachtwoord</label>
-                <input type="password" class="form-control" id="inputPassword" name="inputPassword" placeholder="Wachtwoord">
-            </div>
-            <div class="form-group">
-                <label class="checkbox">
-                    <input type="checkbox"> Onthoud mij
-                </label>
-            </div>
-            <div class="form-group">
-                <button type="submit" class="button">Log in</button>
-            </div>
-        </form>
-    </div>
+    @include('login-dialog')
 
     @section('javascripts')
         <script src="javascripts/jquery-1.10.1.min.js"></script>
