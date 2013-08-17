c06dfebc268f6ade04a425053e7d36a84f549797
Mark Redeman <MarkRedeman@gmail.com>
2013-08-17 16:39:55 +0200

    Partials gebruikt in /gallery/albums

---
+++ laravel/app/views/gallery/_album.blade.php
@@ -0,0 +1,10 @@
+<div class="photo-tile {{ $first }} {{ $wide }}">
+    <a href="fotos.html" class="photo-link" title="{{ $album->name }}">
+        @if ($wide == 'wide')
+            <img src="{{ $album->wideImage() }}" alt="{{ $album->name }}" />
+        @else
+            <img src="{{ $album->smallImage() }}" alt="{{ $album->name }}" />
+        @endif
+        <p class="photo-description">{{ $album->description }}</p>
+    </a>
+</div>
--- laravel/app/views/gallery/albums.blade.php
+++ laravel/app/views/gallery/albums.blade.php
@@ -4,72 +4,22 @@
 		<p class="delta">Krijg een impressie van de meest geweldige studentenvereniging zus en zo en bla die bla. En zus en zo!</p>
 		<div class="albums">
 			<div class="photo-grid-row">
-				<div class="photo-tile first">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[0]->name }}">
-						<img src="{{ $albums[0]->smallImage() }}" alt="{{ $albums[0]->name }}" />
-						<p class="photo-description">{{ $albums[0]->description }}</p>
-					</a>
-				</div>
-				<div class="photo-tile">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[1]->name }}">
-						<img src="{{ $albums[1]->smallImage() }}" alt="{{ $albums[1]->name }}" />
-						<p class="photo-description">{{ $albums[1]->description }}</p>
-					</a>
-				</div>
-				<div class="photo-tile">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[2]->name }}">
-						<img src="{{ $albums[2]->smallImage() }}" alt="{{ $albums[2]->name }}" />
-						<p class="photo-description">{{ $albums[2]->description }}</p>
-					</a>
-				</div>
+				@include('gallery._album', array('album' => $albums[0], 'first' => 'first', 'wide' => ''))
+				@include('gallery._album', array('album' => $albums[1], 'first' => '', 'wide' => ''))
+				@include('gallery._album', array('album' => $albums[2], 'first' => '', 'wide' => ''))
 			</div>
 			<div class="photo-grid-row">
-				<div class="photo-tile first wide">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[3]->name }}">
-						<img src="{{ $albums[3]->wideImage() }}" alt="{{ $albums[3]->name }}" />
-						<p class="photo-description">{{ $albums[3]->description }}</p>
-					</a>
-				</div>
-				<div class="photo-tile">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[4]->name }}">
-						<img src="{{ $albums[4]->smallImage() }}" alt="{{ $albums[4]->name }}" />
-						<p class="photo-description">{{ $albums[4]->description }}</p>
-					</a>
-				</div>
+				@include('gallery._album', array('album' => $albums[4], 'first' => 'first', 'wide' => 'wide'))
+				@include('gallery._album', array('album' => $albums[5], 'first' => '', 'wide' => ''))
 			</div>
 			<div class="photo-grid-row">
-				<div class="photo-tile first">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[5]->name }}">
-						<img src="{{ $albums[5]->smallImage() }}" alt="{{ $albums[5]->name }}" />
-						<p class="photo-description">{{ $albums[5]->description }}</p>
-					</a>
-				</div>
-				<div class="photo-tile">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[6]->name }}">
-						<img src="{{ $albums[6]->smallImage() }}" alt="{{ $albums[6]->name }}" />
-						<p class="photo-description">{{ $albums[6]->description }}</p>
-					</a>
-				</div>
-				<div class="photo-tile">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[7]->name }}">
-						<img src="{{ $albums[7]->smallImage() }}" alt="{{ $albums[7]->name }}" />
-						<p class="photo-description">{{ $albums[7]->description }}</p>
-					</a>
-				</div>
+				@include('gallery._album', array('album' => $albums[5], 'first' => 'first', 'wide' => ''))
+				@include('gallery._album', array('album' => $albums[6], 'first' => '', 'wide' => ''))
+				@include('gallery._album', array('album' => $albums[7], 'first' => '', 'wide' => ''))
 			</div>
 			<div class="photo-grid-row">
-				<div class="photo-tile first">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[8]->name }}">
-						<img src="{{ $albums[8]->smallImage() }}" alt="{{ $albums[8]->name }}" />
-						<p class="photo-description">{{ $albums[8]->description }}</p>
-					</a>
-				</div>
-				<div class="photo-tile wide">
-					<a href="fotos.html" class="photo-link" title="{{ $albums[9]->name }}">
-						<img src="{{ $albums[9]->wideImage() }}" alt="{{ $albums[9]->name }}" />
-						<p class="photo-description">{{ $albums[9]->description }}</p>
-					</a>
-				</div>
+				@include('gallery._album', array('album' => $albums[8], 'first' => 'first', 'wide' => ''))
+				@include('gallery._album', array('album' => $albums[9], 'first' => '', 'wide' => 'wide'))
 			</div>
 		</div>
 
