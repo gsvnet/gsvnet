bdacaabe24a1b76e5cd81b463825825a3c26d913
Mark Redeman <MarkRedeman@gmail.com>
2013-09-09 20:58:47 +0200

    Event data types toegevoegd

--- laravel/app/config/administrator/events.php
+++ laravel/app/config/administrator/events.php
@@ -6,16 +6,35 @@ return array(
 	'model' => 'Model\Event',
 
 	'columns' => array(
+		'title' => array(
+			'title' => 'Titel'
+		),
+		'description' => array(
+			'title' => 'Omschrijving'
+		),
+		'end_date' => array(
+			'title' => 'Eind datum'
+		),
 		'start_date' => array(
-			'title' => 'Begin'
+			'title' => 'Begin datum'
 		)
 	),
 
 	'edit_fields' => array(
+		'title' => array(
+			'title' => 'Titel',
+		),
+		'description' => array(
+			'title' => 'Omschrijving',
+		),
 		'start_date' => array(
-			'title' => 'Begin',
+			'title' => 'Begin datum',
 			'type' => 'date'
-		)
+		),
+		'end_date' => array(
+			'title' => 'Eind datum',
+			'type' => 'date'
+		),
 	)
 );
 ?>
