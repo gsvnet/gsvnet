fd9c8c1ae5508d88b28300a9db8520051a129aaa
Harmen Stoppels <harmenstoppels@gmail.com>
2013-09-28 15:07:45 +0200

    Filter voor commissies werkt
    Met een javascriptplugin die ook indext en daarom erg snel is (:

--- laravel/app/views/de-gsv/committees/show.blade.php
+++ laravel/app/views/de-gsv/committees/show.blade.php
@@ -19,19 +19,23 @@
             </ul>
         </div>
 
-        <div class="secondary-column">
-            <form method="POST" action="/login">
-                <div class="form-group">
-                    <label class="control-label" for="inputSearch">Zoeken</label>
-                    <input type="text" class="form-control" id="inputSearch" name="inputSearch" placeholder="Zoeken">
-                </div>
-            </form>
-            <ul class="secondary-menu">
+        <div id="committees" class="secondary-column">
+            <div class="form-group">
+                <label class="control-label" for="inputSearch">Zoeken</label>
+                <input type="text" class="search form-control" id="inputSearch" name="inputSearch" placeholder="Zoeken">
+            </div>
+            <ul class="list secondary-menu">
                 @foreach ($committees as $committee)
-                    <li><a href="{{ URL::route('show_committee', $committee->id) }}">{{ $committee->name }}</a></li>
+                    <li><a class="committee" href="{{ URL::route('show_committee', $committee->id) }}">{{ $committee->name }}</a></li>
                 @endforeach
             </ul>
         </div>
 
     </div>
+@stop
+
+@section('javascripts')
+    @parent
+    <script src="/javascripts/list.min.js"></script>
+    <script src="/javascripts/committees.js"></script>
 @stop
---
+++ laravel/public/javascripts/committees.js
@@ -0,0 +1,3 @@
+var list = new List('committees', {
+	valueNames: ['committee']
+})
