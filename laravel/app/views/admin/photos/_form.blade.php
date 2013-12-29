b1043dc4d2b865842566c64c2115f1438899480a
Mark <MarkRedeman@gmail.com>
2013-12-29 15:21:53 +0100

    Show url aangepast

--- laravel/app/views/admin/photos/_form.blade.php
+++ laravel/app/views/admin/photos/_form.blade.php
@@ -1,3 +1,3 @@
 {{ Former::text('name')->autofocus() }}
 
-{{ Former::file('photo')->required()->accept('image') }}
+{{ Former::file('photo')->required()->accept('image')->help('Een afbeelding met een minimale dimensie van 634 x 308 wordt aangeraden.') }}
