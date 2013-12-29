db5242ff5c9d41396f91e056347021891c86cb8b
Mark <MarkRedeman@gmail.com>
2013-12-29 16:35:50 +0100

    Het foto formulier laat nu zien wat de dimensies van een foto moeten zijn

--- laravel/app/views/admin/photos/_form.blade.php
+++ laravel/app/views/admin/photos/_form.blade.php
@@ -1,3 +1,3 @@
 {{ Former::text('name')->autofocus() }}
 
-{{ Former::file('photo')->required()->accept('image')->help('Een afbeelding met een minimale dimensie van 634 x 308 wordt aangeraden.') }}
+{{ Former::file('photo')->required()->accept('image')->help("Een afbeelding met een minimale dimensie van 634 x 308 wordt aangeraden. Foto's groter dan 1024 x 768 worden verkleind.") }}
