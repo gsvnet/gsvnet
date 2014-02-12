143888b9df6406930b26c5ddeb535723b1c012d2
Mark <MarkRedeman@gmail.com>
2014-02-12 14:04:52 +0100

    Maak album publiek

--- laravel/app/views/admin/albums/_form.blade.php
+++ laravel/app/views/admin/albums/_form.blade.php
@@ -1,4 +1,4 @@
 <div>{{ Former::text('name')->autofocus() }}</div>
 <div>{{ Former::textarea('description') }}</div>
 
-<div>{{ Former::checkbox('public')->text('Publiceer pagina')->label(null) }}</div>
+<div>{{ Former::checkbox('public')->text('Maak album pulbiek')->label(null) }}</div>
