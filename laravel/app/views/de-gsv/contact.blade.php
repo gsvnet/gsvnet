0f343890b4cbb283f23e6e5786cd265b81a77146
Harmen Stoppels <harmenstoppels@gmail.com>
2014-02-16 23:16:39 +0100

    Google Maps toegevoegd aan contact

--- laravel/app/views/de-gsv/contact.blade.php
+++ laravel/app/views/de-gsv/contact.blade.php
@@ -5,4 +5,11 @@
 
         <p>Maak nu contact met ons</p>
     </article>
+    
+    <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=nl&amp;geocode=&amp;q=Hereweg+40,+Herewegbuurt,+Groningen,+Nederland&amp;aq=0&amp;oq=hereweg+40&amp;sll=37.0625,-95.677068&amp;sspn=63.255964,135.263672&amp;ie=UTF8&amp;hq=&amp;hnear=Hereweg+40,+9725+AE+Herewegwijk+en+Helpman,+Groningen,+Nederland&amp;t=m&amp;z=14&amp;ll=53.209373,6.572308&amp;output=embed"></iframe>
+
+@stop
+
+@section('word-lid')
+
 @stop
