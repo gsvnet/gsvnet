104ac8d3dcdc2a25d928bd29e1145fb61dfe55f0
Mark <MarkRedeman@gmail.com>
2014-02-19 14:50:29 +0100

    Cache dingen

--- laravel/app/controllers/PhotoController.php
+++ laravel/app/controllers/PhotoController.php
@@ -80,6 +80,43 @@ class PhotoController extends BaseController {
     {
         $photo = $this->photos->byId($id);
         $image = $this->imageHandler->get($photo->src_path, $type);
-        return $image->response();
+        $response = $image->response();
+
+        $path = $this->imageHandler->getStoragePath($photo->src_path, $type);
+        $name = $photo->name;
+
+         if (is_null($name)) {
+            $name = basename($path);
+        }
+
+        $filetime = filemtime($path);
+        $etag = md5($filetime . $path);
+        $time = gmdate('r', $filetime);
+        // Keep images 1 month
+        $lifetime = 60*60*24*30;
+        $expires = gmdate('r', $filetime + $lifetime);
+        // $expires = '+1 month';
+        $length = filesize($path);
+
+        $headers = array(
+            'Content-Disposition' => 'inline; filename="' . $name . '"',
+            'Last-Modified' => $time,
+            'Cache-Control' => 'must-revalidate',
+            'Expires' => $expires,
+            'Pragma' => 'public',
+            'Etag' => $etag,
+        );
+        $headerTest1 = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && $_SERVER['HTTP_IF_MODIFIED_SINCE'] == $time;
+        $headerTest2 = isset($_SERVER['HTTP_IF_NONE_MATCH']) && str_replace('"', '', stripslashes($_SERVER['HTTP_IF_NONE_MATCH'])) == $etag;
+        //image is cached by the browser, we dont need to send it again
+        if ($headerTest1 || $headerTest2) {
+            return Response::make('', 304, $headers);
+        }
+
+        foreach ($headers as $header => $value) {
+            $response->header($header, $value);
+        }
+
+        return $response;
     }
 }
