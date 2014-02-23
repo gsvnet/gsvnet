0597bad98241bcba022e0940bba9e25956444680
Mark <MarkRedeman@gmail.com>
2014-02-23 15:57:46 +0100

    Event show pagina aangepast zodat deze laat zien of een activiteit prive is

--- laravel/app/views/events/show.blade.php
+++ laravel/app/views/events/show.blade.php
@@ -12,13 +12,17 @@
                 {{ $event->description }}
             </p>
 
-            @if(!empty($event->location))
+            @if (! empty($event->location))
             <strong>Locatie: {{ $event->location }}</strong>
+            @endif
+
+            @if (! $event->public)
+                <strong>Prive activiteit</strong>
             @endif
         </div>
 
         <div class="secondary-column">
-            @if(isset($types[$event->type]))
+            @if (isset($types[$event->type]))
                 <div class="single-event-details {{$types[$event->type]}}">
                     Dit is nou typische een activiteit met zo'n kleur
                 </div>
