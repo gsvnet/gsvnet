496871b505ab79540f9ec54bb673b86a36abe12a
Mark <MarkRedeman@gmail.com>
2014-02-25 18:13:01 +0100

    Register form in een partial view gezet

--- laravel/app/views/word-lid/login-or-register.blade.php
+++ laravel/app/views/word-lid/login-or-register.blade.php
@@ -27,13 +27,9 @@
             {{ Former::open()->action(URL::action('UserController@postRegister')) }}
                 <h2>Registreer</h2>
                 <p>Heb je nog geen account? Maak er een aan!</p>
-                {{ Former::text('register-username')->label('Gebruikersnaam')}}
-                {{ Former::text('register-firstname')->label('Voornaam')}}
-                {{ Former::text('register-middlename')->label('Tussenvoegsel')->size('3')}}
-                {{ Former::text('register-lastname')->label('Achternaam')}}
-                {{ Former::text('register-email')->label('Email')}}
-                {{ Former::password('register-password')->label('Wachtwoord')}}
-                {{ Former::password('register-password_confirmation')->label('Herhaal wachtwoord')}}
+
+                @include('register._form')
+
                 <input type="hidden" name="become-member-register" value="1">
                 <div class="form-group">
                     <button type="submit" class="button">Registreer</button>
