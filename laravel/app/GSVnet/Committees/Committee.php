c64d5e00b8a3522ed8bfb5465e6cac5063568711
Mark <MarkRedeman@gmail.com>
2014-02-25 12:28:49 +0100

    Committe->activeMembers geeft ook members die geen eind datum hebben

--- laravel/app/GSVnet/Committees/Committee.php
+++ laravel/app/GSVnet/Committees/Committee.php
@@ -16,14 +16,12 @@ class Committee extends \Eloquent {
         // Select all active members, i.e. for which the current date is
         //  between the start and enddate
         return $this->belongsToMany('GSVnet\Users\User', 'committee_user')
-            ->wherePivot('start_date', '<=', new \DateTime('now'))
-            ->wherePivot('end_date', '>=', new \DateTime('now'))
-
-            // ->where(function($q){
-            //     return $q->wherePivot('end_date', '>=', new \DateTime('now'))
-            //     ->orWherePivot('end_date', '<>', 'NULL');
-
-            // })
+            ->where('committee_user.start_date', '<=', new \DateTime('now'))
+            ->where(function($q) {
+                $date = new \DateTime('now');
+                return $q->where('committee_user.end_date', '>=', new \DateTime('now'))
+                    ->orWhereNull('committee_user.end_date');
+            })
             ->withPivot('start_date', 'end_date');
     }
 
