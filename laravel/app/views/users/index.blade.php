644ea445e355fcf3b705543c4d5fc8b4166d33aa
Mark <MarkRedeman@gmail.com>
2014-02-26 23:31:31 +0100

    Gravatars op jaarbundel pagina worden juist weergeven

--- laravel/app/views/users/index.blade.php
+++ laravel/app/views/users/index.blade.php
@@ -39,21 +39,21 @@
 				@foreach ($members as $member)
 					<li class="user-profile-item">
 						<div class="profile-image">
-							{{ Gravatar::image(Auth::user()->email, 'Profielfoto', array('width' => 120, 'height' => 120)) }}
+							{{ Gravatar::image($member->user->email, 'Profielfoto', array('width' => 120, 'height' => 120)) }}
 						</div>
 						<div class="user-details">
 							<h3>{{ link_to_action('UserController@showUser', $member->user->full_name, array($member->id, 'class' => 'search-users')) }}</h3>
 							<ul class="user-details-list">
 								<li>
-									<i class="fa fa-share"></i> 
+									<i class="fa fa-share"></i>
 									{{ HTML::mailto($member->user->email) }}
 								</li>
 								<li class="phone">
-									<i class="fa fa-phone"></i> 
+									<i class="fa fa-phone"></i>
 									<a href="tel:{{{$member->phone}}}" title="Bel {{{$member->user->full_name}}}">{{{$member->phone}}}</a>
 								</li>
 								<li>
-									<span class="dot-after">{{{$member->yearGroup->name or 'Geen jaarverband'}}}</span> 
+									<span class="dot-after">{{{$member->yearGroup->name or 'Geen jaarverband'}}}</span>
 									Regio {{{$member->region}}}
 								</li>
 							</ul>
