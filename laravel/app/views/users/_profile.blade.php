539d3baa760bca99a5e93dabdb165bc3f0738e1a
Mark <MarkRedeman@gmail.com>
2014-02-27 22:53:17 +0100

    Partial gemaakt voor jaarbundel profiel

---
+++ laravel/app/views/users/_profile.blade.php
@@ -0,0 +1,22 @@
+<li class="user-profile-item">
+    <div class="profile-image">
+        {{ Gravatar::image($member->user->email, 'Profielfoto', array('width' => 120, 'height' => 120)) }}
+    </div>
+    <div class="user-details">
+        <h3>{{ link_to_action('UserController@showUser', $member->user->full_name, [$member->id], ['class' => 'search-users']) }}</h3>
+        <ul class="user-details-list">
+            <li>
+                <i class="fa fa-share"></i>
+                {{ HTML::mailto($member->user->email) }}
+            </li>
+            <li class="phone">
+                <i class="fa fa-phone"></i>
+                <a href="tel:{{{$member->phone}}}" title="Bel {{{$member->user->full_name}}}">{{{$member->phone}}}</a>
+            </li>
+            <li>
+                <span class="dot-after">{{{$member->yearGroup->name or 'Geen jaarverband'}}}</span>
+                Regio {{{$member->region}}}
+            </li>
+        </ul>
+    </div>
+</li>
