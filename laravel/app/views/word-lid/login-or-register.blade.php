3f89429f1843590e29874c14f8035ff80bdef091
Harmen Stoppels <harmenstoppels@gmail.com>
2014-02-27 14:46:17 +0100

    Kolommen ge-css-module-achtigd

--- laravel/app/views/word-lid/login-or-register.blade.php
+++ laravel/app/views/word-lid/login-or-register.blade.php
@@ -4,21 +4,7 @@
         <p class="delta">Voordat je je aan kunt melden bij de Gereformeerde Studentenvereniging Groningen, moet je eerst een account aanmaken op de website</p>
     </div>
     <div class="column-holder">
-        <div id="login-form" class="login-form">
-            {{ Former::open()->action(action('SessionController@postLogin')) }}
-                <h2>Login</h2>
-                <p>Heb je al een bestaand account? Log dan in!</p>
-
-                @include('partials._login')
-
-                <input type="hidden" name="become-member-login" value="1">
-                <div class="form-group">
-                    <button type="submit" class="button">Log in</button>
-                </div>
-            {{ Former::close() }}
-        </div>
-
-        <div id="register-form" class="register-form">
+        <div id="register-form" class="half-column">
             {{ Former::open()->action(URL::action('RegisterController@create')) }}
                 <h2>Registreer</h2>
                 <p>Heb je nog geen account? Maak er een aan!</p>
@@ -30,5 +16,19 @@
                     <button type="submit" class="button">Registreer</button>
                 </div>
             {{Former::close()}}
+        </div>
+
+        <div id="login-form" class="half-column with-border">
+            {{ Former::open()->action(action('SessionController@postLogin')) }}
+                <h2>Login</h2>
+                <p>Heb je al een bestaand account? Log dan in!</p>
+
+                @include('partials._login')
+
+                <input type="hidden" name="become-member-login" value="1">
+                <div class="form-group">
+                    <button type="submit" class="button">Log in</button>
+                </div>
+            {{ Former::close() }}
         </div>
     </div>
