11ddda4c1bb770083c3f73d5de619ced63d70a6f
Harmen Stoppels <harmenstoppels@gmail.com>
2014-03-04 10:11:37 +0100

    Markdown toegevoegd

---
+++ laravel/app/GSVnet/MarkDown/HtmlMarkdownConvertor.php
@@ -0,0 +1,28 @@
+<?php namespace GSVnet\Markdown;
+
+use HTML_To_Markdown;
+
+class HtmlMarkdownConvertor
+{
+    protected $htmlParser;
+    protected $markdownParser;
+
+    public function __construct()
+    {
+        $this->htmlParser = new HTML_To_markdown;
+        $this->htmlParser->set_option('header_style', 'atx');
+
+        $this->markdownParser = new \Michelf\MarkdownExtra;
+        $this->markdownParser->no_markup = true;
+    }
+
+    public function convertHtmlToMarkdown($html)
+    {
+        return $this->htmlParser->convert($html);
+    }
+
+    public function convertMarkdownToHtml($markdown)
+    {
+        return $this->markdownParser->transform($markdown);
+    }
+}
