d36c69ab82c9bea879792bfbf743c214fe7134e0
Harmen Stoppels <harmenstoppels@gmail.com>
2014-03-06 00:25:31 +0100

    Login dialog

--- laravel/app/views/login-dialog.blade.php
+++ laravel/app/views/login-dialog.blade.php
@@ -1,18 +1,21 @@
-<div id="login-dialog" class="zoom-anim-dialog mfp-hide">
-    <h2>Login</h2>
+<div id="login-dialog" class="dialog zoom-anim-dialog mfp-hide">
+    <div class="dialog-main">
+        <h2>Login</h2>
 
-    {{ Former::open()->action(action('SessionController@postLogin')) }}
+        {{ Former::open()->action(action('SessionController@postLogin')) }}
 
-        <p>Inloggen geeft je toegang tot het forum</p>
+            <p>Inloggen geeft je toegang tot het forum</p>
 
 
-        @include('partials._login')
+            @include('partials._login')
 
-        <div class="form-group">
-            <button type="submit" class="button">Log in</button>
-        </div>
-    {{ Former::close() }}
-
-    <h3>Geen account?</h3>
-    {{ link_to_action('RegisterController@create', 'Registreer je dan nu!') }}
+            <div class="form-group">
+                <button type="submit" class="button">Log in</button>
+            </div>
+        {{ Former::close() }}
+    </div>
+    <div class="dialog-footer">
+        <h3>Geen account?</h3>
+        {{ link_to_action('RegisterController@create', 'Registreer je dan nu!') }}
+    </div>
 </div>
