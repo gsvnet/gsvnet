85a9566a116190c3278580116dd2befa722ff56c
Mark <MarkRedeman@gmail.com>
2014-03-13 14:37:25 +0100

    EntityNotFoundException extends ModelNotFoundException

--- laravel/app/GSVnet/Core/Exceptions/EntityNotFoundException.php
+++ laravel/app/GSVnet/Core/Exceptions/EntityNotFoundException.php
@@ -1,3 +1,5 @@
 <?php namespace GSVnet\Core\Exceptions;
+// Extend the Model Not Found Exception such that we can use  App::error($e ModelNotFoundException)
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
-class EntityNotFoundException extends \Exception {}
+class EntityNotFoundException extends ModelNotFoundException {}
