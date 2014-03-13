31ae40afe386cede445aafcfef4f3b59f58e4bc4
Harmen Stoppels <harmenstoppels@gmail.com>
2014-03-13 14:39:34 +0100

    Beste seeds ooit!

--- laravel/app/database/seeds/ForumTableSeeder.php
+++ laravel/app/database/seeds/ForumTableSeeder.php
@@ -1,26 +1,37 @@
 <?php
 
-class ForumTableSeeder extends Seeder implements GSVnet\Forum\Threads\ThreadCreatorListener {
+class ForumTableSeeder extends Seeder implements 
+    GSVnet\Forum\Threads\ThreadCreatorListener,
+    GSVnet\Forum\Replies\ReplyCreatorListener {
 
     public function run()
     {
         DB::table('forum_threads')->truncate();
+        DB::table('forum_replies')->truncate();
 
         $faker = Faker\Factory::create();
-        //$faker->addProvider(new Faker\Provider\Text($faker));
+        $users = GSVnet\Users\User::orderBy(DB::raw('RAND()'))->get();
+        $numUsers = count($users);
+        $numThreads = 30;
+        $maxReplies = 300;
 
-        $users = GSVnet\Users\User::orderBy(DB::raw('RAND()'))->take(5)->get();
-
-        foreach($users as $user)
+        for($j=0; $j<$numThreads; $j++)
         {
         	$tags = GSVnet\Tags\Tag::orderBy(DB::raw('RAND()'))->take(rand(1,3))->get();
-	        App::make('GSVnet\Forum\Threads\ThreadCreator')->create($this, [
+	        $thread = App::make('GSVnet\Forum\Threads\ThreadCreator')->create($this, [
 	            'subject' => $faker->text(20),
 	            'body' => $faker->text(500),
-	            'author' => $user,
+	            'author' => $users[rand(0, $numUsers-1)],
 	            'tags' => $tags
 	        ]);
-        	
+
+            // Willekeurige users geven willekeurige reacties
+            for ($i=0; $i < rand(0, $maxReplies); $i++) { 
+                App::make('GSVnet\Forum\Replies\ReplyCreator')->create($this, [
+                    'body'   => $faker->text(rand(10, 800)),
+                    'author' => $users[rand(0, $numUsers-1)],
+                ], $thread->id);
+            }
         }
     }
 
@@ -32,7 +43,18 @@ class ForumTableSeeder extends Seeder implements GSVnet\Forum\Threads\ThreadCrea
 
     public function threadCreated($thread)
     {
-    	//dd($thread);
+    	return $thread;
+    }
+
+    // Gewoon even wat functies
+    public function replyCreationError($errors)
+    {
+        dd($errors);
+    }
+
+    public function replyCreated($reply)
+    {
+        return $reply;
     }
 
 }
