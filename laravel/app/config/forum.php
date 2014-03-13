bb76bde8b3775e3d5473a70088c411b1b48d10bc
Harmen Stoppels <harmenstoppels@gmail.com>
2014-03-13 19:54:41 +0100

    Tags in de sidebar

--- laravel/app/config/forum.php
+++ laravel/app/config/forum.php
@@ -4,6 +4,21 @@ return [
     'sections' => [
         'Alle onderwerpen' => [
             'tags' => '',
+        ],
+        'Discussies' => [
+        	'tags' => 'jaarverbanden,regios'
+        ],
+        'GSV / Senaat' => [
+        	'tags' => 'gsv,senaat'
+        ],
+        'Jaarverbanden / Regio\'s' => [
+        	'tags' => 'jaarverbanden,regios'
+        ],
+        'Maatschappij / Politiek' => [
+        	'tags' => 'maatschappij-en-politiek'
+        ],
+        'Geloof / Wetenschap' => [
+        	'tags' => 'geloof-en-wetenschap'
         ]
     ]
 ];
