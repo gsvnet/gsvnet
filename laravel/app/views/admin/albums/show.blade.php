a1b76bdd37b923d38481636872328c53e377d10c
Harmen Stoppels <harmenstoppels@gmail.com>
2014-03-16 21:33:40 +0100

    zonder border is mooier

--- laravel/app/views/admin/albums/show.blade.php
+++ laravel/app/views/admin/albums/show.blade.php
@@ -37,7 +37,7 @@
 
                                 <div class="panel panel-default">
                                     <div class="panel-body">
-                                        <a href="{{ URL::action('Admin\PhotoController@show', [$album->id, $photo->id]) }}" alt="{{{ $photo->name }}}" class="thumbnail">
+                                        <a href="{{ URL::action('Admin\PhotoController@show', [$album->id, $photo->id]) }}" alt="{{{ $photo->name }}}">
                                           <img src="{{ $photo->small_image_url }}" alt="{{{ $photo->name }}}">
                                         </a>
                                     </div>
