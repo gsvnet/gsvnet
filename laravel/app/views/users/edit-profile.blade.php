ce47c2a886e239d1a480497b1a6fc126e98a1dce
Harmen Stoppels <harmenstoppels@gmail.com>
2014-04-04 17:06:27 +0200

    profiel bewerken nu alleen voor leden

--- laravel/app/views/users/edit-profile.blade.php
+++ laravel/app/views/users/edit-profile.blade.php
@@ -20,41 +20,43 @@
 			{{ Former::text('username')->label('Gebruikersnaam')->disabled() }}
 			{{ Former::text('email')->label('Email') }}
 
-			<h2>Vul vooral meer in</h2>
-			{{ Former::text('profile[church]')->label('Kerkgezindte') }}
-            {{ Former::text('profile[study]')->label('Naam van studie') }}
-            {{ Former::select('profile[start_date_rug]')->range(date('Y')+1, date('Y')-4)->label('Jaar waarin je begon of begint met studeren') }}
+			@if(Permission::has('users.edit-profile'))
 
-            {{ Former::text('profile[address]')->label('Adres') }}
-            {{ Former::text('profile[zip_code]')->label('Postcode')->size(6) }}
-            {{ Former::text('profile[town]')->label('Woonplaats') }}
-            {{ Former::text('profile[phone]')->label('Telefoon') }}
-            {{ Former::select('profile[gender]')->label('Geslacht')->options(array('male' => 'Man', 'female' => 'Vrouw')) }}
+				<h2>Vul vooral meer in</h2>
+				{{ Former::text('profile[church]')->label('Kerkgezindte') }}
+	            {{ Former::text('profile[study]')->label('Naam van studie') }}
+	            {{ Former::select('profile[start_date_rug]')->range(date('Y')+1, date('Y')-4)->label('Jaar waarin je begon of begint met studeren') }}
 
-            <h2>Adres van je ouders</h2>
-            {{
-                Former::stacked_radios('parents-same-address')->radios(array(
-                    'Ja' => array('name' => 'parents-same-address', 'value' => '1'),
-                    'Nee' => array('name' => 'parents-same-address', 'value' => '0'),
-                ))->label('Woon je bij je ouders/verzorgers?')->check('0')
-            }}
-            <div id="parents-info">
-    			{{ Former::text('profile[parent_address]')->label('Adres ouders') }}
-                {{ Former::text('profile[parent_zip_code]')->label('Postcode ouders')->size(6) }}
-                {{ Former::text('profile[parent_town]')->label('Woonplaats ouders') }}
-            </div>
+	            {{ Former::text('profile[address]')->label('Adres') }}
+	            {{ Former::text('profile[zip_code]')->label('Postcode')->size(6) }}
+	            {{ Former::text('profile[town]')->label('Woonplaats') }}
+	            {{ Former::text('profile[phone]')->label('Telefoon') }}
+	            {{ Former::select('profile[gender]')->label('Geslacht')->options(array('male' => 'Man', 'female' => 'Vrouw')) }}
+
+	            <h2>Adres van je ouders</h2>
+	            {{
+	                Former::stacked_radios('parents-same-address')->radios(array(
+	                    'Ja' => array('name' => 'parents-same-address', 'value' => '1'),
+	                    'Nee' => array('name' => 'parents-same-address', 'value' => '0'),
+	                ))->label('Woon je bij je ouders/verzorgers?')->check('0')
+	            }}
+	            <div id="parents-info">
+	    			{{ Former::text('profile[parent_address]')->label('Adres ouders') }}
+	                {{ Former::text('profile[parent_zip_code]')->label('Postcode ouders')->size(6) }}
+	                {{ Former::text('profile[parent_town]')->label('Woonplaats ouders') }}
+	            </div>
 
                 {{ Former::text('profile[parent_phone]')->label('Telefoon ouders') }}
+	        @endif
 		</div>
 		<div class="secondary-column">
-			<h2>Je profielfoto</h2>
-            {{ Former::file('photo')->label('Upload een foto van jezelf')->accept('image') }}
-			<div style="height:100px;background:grey;">
-
-			</div>
+			@if(Permission::has('users.edit-profile'))
+				<h2>Je profielfoto</h2>
+	            {{ Former::file('photo')->label('Upload een foto van jezelf')->accept('image') }}
+	        @endif
 
 			<h2>Je avatar</h2>
-			<p>Je avatar kun je aanpassen op gravatar</p>
+			<p>Je avatar kun je aanpassen op <a href="http://nl.gravatar.com/" title="Je avatar aanpassen">Gravatar</a>.</p>
 
 			<h2>Dingen die je niet kunt veranderen</h2>
 			<p>Als je je naam, verjaardag of iets anders wat je niet kunt veranderen toch per se wil veranderen, dan moet je even contact op nemen met de abactis.</p>
