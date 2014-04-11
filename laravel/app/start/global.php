f521300f8845cfa45eb6ed6340bb159a328a0469
Mark <MarkRedeman@gmail.com>
2014-04-11 17:07:01 +0200

    Helpers bestand toegevoegd met inlinePhoto macro

--- laravel/app/start/global.php
+++ laravel/app/start/global.php
@@ -90,3 +90,11 @@ require app_path().'/filters.php';
 */
 
 require app_path().'/errors.php';
+
+/*
+|--------------------------------------------------------------------------
+| Require The Helpers File
+|--------------------------------------------------------------------------
+*/
+
+require app_path().'/helpers.php';
