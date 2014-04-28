3a8587c4769a96f3b3ef49c8391aeb869c647949
Harmen Stoppels <harmenstoppels@gmail.com>
2014-04-29 00:10:43 +0200

    config

---
+++ laravel/app/config/packages/mews/purifier/config.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of HTMLPurifier Bundle.
+ * (c) 2012 Maxime Dizerens
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return array(
+	'encoding' => 'UTF-8',
+    'finalize' => true,
+    'preload'  => false,
+    'settings' => array(
+        'default' => array(
+            'HTML.Doctype'             => 'XHTML 1.0 Strict',
+            'HTML.Allowed'             => 'div,b,blockquote,cite,strong,i,em,a[href|title],ul,ol,li,p[style],br,span[style],img[width|height|alt|src]',
+            'AutoFormat.AutoParagraph' => false,
+            'AutoFormat.RemoveEmpty'   => true,
+        ),
+    ),
+);
