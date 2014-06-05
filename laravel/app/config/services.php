72c5e6e9f4db6905fc7952195df28d6985521d36
Harmen Stoppels <harmenstoppels@gmail.com>
2014-06-05 17:15:38 +0200

    nog meer credentials veilig

--- laravel/app/config/services.php
+++ laravel/app/config/services.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	'mailgun' => array(
-		'domain' => 'sandbox81735.mailgun.org',
-		'secret' => 'key-2c5m7zqm0ysembrt9g-fmzppkjxnpi37',
+		'domain' => $_ENV['mailgun.domain'],
+		'secret' => $_ENV['mailgun.secret']
 	),
 );
