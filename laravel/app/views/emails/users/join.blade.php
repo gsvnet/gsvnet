cea677e3e4389ee6529316fd7e3fa0ba4b824283
Harmen Stoppels <harmenstoppels@gmail.com>
2014-06-06 18:09:36 +0200

    tikfout

--- laravel/app/views/emails/users/join.blade.php
+++ laravel/app/views/emails/users/join.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <h2>Hoi {{{ $fullname }}}</h2>
-    <p>Je hebt zojuist een goede keuzen in je leven gemaakt door je officieel aan te melden bij de GSV!</p>
+    <p>Je hebt zojuist een fantastische keuze in je leven gemaakt door je officieel aan te melden bij de GSV!</p>
     <p>De Novcie zal zo spoedig mogelijk contact met je opnemen en je meer informatie geven over het novitiaat.</p>
     <p>Vriendelijke groeten,</p>
     <p>De Webcie</p>
