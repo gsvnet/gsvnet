1e281a94d2a886d070e9b66e17981093378c5d85
Harmen Stoppels <harmenstoppels@gmail.com>
2014-06-22 12:40:46 +0200

    betere plek knop'

--- laravel/app/views/forum/replies/_create.blade.php
+++ laravel/app/views/forum/replies/_create.blade.php
@@ -2,6 +2,7 @@
     {{ Former::textarea('body')->placeholder('Tekst')->label('Reageer')->rows(10) }}
     
     <div class="control-group">
-        <input type="submit" id="submit-reply" value="Reageer" class="button">
+        <input type="submit" id="submit-reply" value="Reageer" class="button right">
+        <a href="{{ URL::action('ForumThreadsController@getIndex') }}" title="Terug naar de laatste topics" class="button disabled">&laquo; Terug</a></p>
     </div>
 {{ Former::close() }}
