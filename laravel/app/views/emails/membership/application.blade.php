1364c18ee89cfa3fe903c793dfa4ff20bd61e153
Harmen Stoppels <harmenstoppels@gmail.com>
2014-07-10 22:49:22 +0200

    link

--- laravel/app/views/emails/membership/application.blade.php
+++ laravel/app/views/emails/membership/application.blade.php
@@ -5,6 +5,6 @@
 @stop
 
 @section('content')
-    <p>Hier info over een nieuwe aanmelding. Zie de <a href="http://gsvnet.nl/admin">adminpagina</a> voor dezelfde info plus een eventuele foto!.</p>
+    <p>Hier info over een nieuwe aanmelding. Zie de <a href="http://gsvnet.nl/admin">adminpagina</a> voor dezelfde info plus een eventuele foto!</p>
     <pre>{{ var_dump($user) }}</pre>
 @stop
