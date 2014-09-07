b5b50a094a6311cf5c7f7c791b83dd438fc04723
Harmen Stoppels <harmenstoppels@gmail.com>
2014-09-07 23:57:30 +0200

    oke

--- laravel/app/views/admin/events/_form.blade.php
+++ laravel/app/views/admin/events/_form.blade.php
@@ -5,7 +5,7 @@
 {{ Former::select('type')->options(Config::get('gsvnet.eventTypesPresented', []))->label('Soort')->required()->help('Dit is voor het plaatje') }}
 {{ Former::date('start_date')->required()->label('Startdatum')->help('jjjj-mm-dd') }}
 {{ Former::checkbox('whole_day')->value('1')->text('Begintijd doet er niet toe')->label(null) }}
-{{ Former::text('start_time')->step('1')->label('Starttijd')->help('uu:mm. Een starttijd is alleen verplicht als je het het vakje hierboven niet is aangevinkt')->help('hh:mm:ss') }}
+{{ Former::text('start_time')->step('1')->label('Starttijd')->help('uu:mm') }}
 {{ Former::date('end_date')->required()->label('Einddatum')->help('jjjj-mm-dd. Als de activiteit maar een dag duurt, moet je dezelfde datum invullen als de startdatum') }}
 
 {{ Former::checkbox('public')->text('Maak activiteit openbaar (voor externen)')->label(null) }}
