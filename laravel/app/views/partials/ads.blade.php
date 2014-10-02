8df3a5e26b5547084cc9e8f64e46b3d2cced1c20
Harmen <harmenstoppels@gmail.com>
2014-10-02 12:50:04 +0200

    responsive

--- laravel/app/views/partials/ads.blade.php
+++ laravel/app/views/partials/ads.blade.php
@@ -1,6 +1,11 @@
 @if(! Permission::has('ads.hide'))
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-6636356080644040" data-ad-slot="4778218464"></ins>
+    <!-- GSVnet responsive -->
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-6636356080644040"
+         data-ad-slot="6695603663"
+         data-ad-format="auto"></ins>
     <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
