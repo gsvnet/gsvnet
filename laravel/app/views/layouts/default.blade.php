60fae86a5cb391e6b0f089788afb0f79063fe037
Harmen <=>
2014-12-17 13:34:16 +0100

    nog een keer

--- laravel/app/views/layouts/default.blade.php
+++ laravel/app/views/layouts/default.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{{ $title or 'GSV' }}}</title>
 
-    <meta name="description" content="{{{ $description or 'Gereformeerde Studenten Vereniging Groningen' }}}" >
+    <meta name="description" content="{{{ !empty($description) ? $description : 'Gereformeerde Studenten Vereniging Groningen' }}}" >
     <meta property="og:site_name" content="Gereformeerde Studenten Vereniging Groningen">
     <meta property="og:title" content="{{{ $title or 'GSV' }}}">
     <meta property="og:description" content="{{{ !empty($description) ? $description : 'Gereformeerde Studenten Vereniging Groningen' }}}">
