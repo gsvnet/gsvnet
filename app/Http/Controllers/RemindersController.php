5cd0039e33e8ae4fd1e4c972f0a3e561ad89de77
Harmen <harmenstoppels@gmail.com>
2015-02-24 18:16:29 +0100

    Niet dubbel encrypten

--- app/Http/Controllers/RemindersController.php
+++ app/Http/Controllers/RemindersController.php
@@ -4,6 +4,7 @@ use Former\Facades\Former;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RemindersController extends BaseController {
@@ -32,5 +33,38 @@ class RemindersController extends BaseController {
             throw new NotFoundHttpException;
 
         return view('password.reset')->with('token', $token);
+    }
+
+    public function postReset(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required',
+            'email' => 'required',
+            'password' => 'required|confirmed',
+        ]);
+
+        $credentials = $request->only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
+
+        $response = $this->passwords->reset($credentials, function($user, $password)
+        {
+            $user->password = $password;
+
+            $user->save();
+
+            $this->auth->login($user);
+        });
+
+        switch ($response)
+        {
+            case PasswordBroker::PASSWORD_RESET:
+                return redirect($this->redirectPath());
+
+            default:
+                return redirect()->back()
+                    ->withInput($request->only('email'))
+                    ->withErrors(['email' => trans($response)]);
+        }
     }
 }
