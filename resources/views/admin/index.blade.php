36b8da03d0fe5bcb9b3a4841fee090d7ebbb79c3
Harmen <harmenstoppels@gmail.com>
2015-02-25 15:45:04 +0100

    Exportknop goed

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
 
     <h2>Download de lijst met leden of oud-leden</h2>
     <p>
-        <a href="{!! URL::action('Admin\UsersController@showMembers') !!}" class="btn btn-sm btn-primary"><i class="fa fa-download"></i> Exporteer leden</a>
-        <a href="{!! URL::action('Admin\UsersController@showFormerMembers') !!}" class="btn btn-sm btn-default"><i class="fa fa-download"></i> Exporteer oud-leden</a>
+        <a href="{!! URL::action('Admin\UsersController@exportMembers') !!}" class="btn btn-sm btn-primary"><i class="fa fa-download"></i> Exporteer leden</a>
+        <a href="{!! URL::action('Admin\UsersController@exportFormerMembers') !!}" class="btn btn-sm btn-default"><i class="fa fa-download"></i> Exporteer oud-leden</a>
     </p>
 @stop
