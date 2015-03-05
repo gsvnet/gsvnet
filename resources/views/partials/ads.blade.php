d5468a47433271befd9908d951373b31addbcfef
Harmen <harmenstoppels@gmail.com>
2015-03-05 12:20:44 +0100

    Reclame

--- resources/views/partials/ads.blade.php
+++ resources/views/partials/ads.blade.php
@@ -1,7 +1,15 @@
 @if(! Permission::has('ads.hide'))
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-6636356080644040" data-ad-slot="4778218464"></ins>
-    <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
+
+    <div style="height:10em;">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- GSVnet forum -->
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-client="ca-pub-6636356080644040"
+             data-ad-slot="9686492062"
+             data-ad-format="auto"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
 @endif
