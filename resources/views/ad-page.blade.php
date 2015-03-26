89ad091b0e38bffaf5aac2f449da01ca0b1cd996
Harmen <harmenstoppels@gmail.com>
2015-03-26 21:02:44 +0100

    Sponsorpagina

---
+++ resources/views/ad-page.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.default')
+
+@section('title', 'Geld verdienen voor de GSV')
+@section('description', 'Zorg ervoor dat de commercie een goed jaar draait')
+@section('body-id', '')
+
+@section('content')
+    <article class="artikel column-holder" role="main">
+        <div>
+            <h1>Sponsors</h1>
+            <p class="lead">De GSV kan centen verdienen als u gebruik maakt van het sponsorprogramma. Dat gaat door aankopen te doen via de speciale links hieronder. Supersimpel.</p>
+        </div>
+
+        <h2>Bol.com</h2>
+        <p>
+            Klikt u op onderstaande magische knop en koopt u een mooi product, dan krijgt de GSV 3 tot 8% van het aankoopbedrag. Specifieke details hierover zijn <a href="https://partnerprogramma.bol.com/partner/commissie.do">hier te vinden</a>.
+        </p>
+        <p>
+            <a href="https://partnerprogramma.bol.com/click/click?p=1&t=url&s=33791&f=TXL&url=http%3A%2F%2Fwww.bol.com&name=GSV-tekstadvertentie" title="Bol.com" class="button">Bol.com</a>
+        </p>
+
+        <p>U kunt de link ook doorsturen naar familie of bekenden <small>als zij toch al een aankoop gaan doen</small>!</p>
+    </article>
+
+@stop
