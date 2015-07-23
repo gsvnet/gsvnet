ccd1173cd4bf5b35449de56d57e61f74da2ba95c
Harmen <harmenstoppels@gmail.com>
2015-07-23 19:49:36 +0200

    versie css/js

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -35,7 +35,7 @@
 
     @section('stylesheets')
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.6.7">
+        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.6.8">
     @show
 </head>
 <body id="@yield('body-id', 'gsvnet')">
@@ -78,7 +78,7 @@
     @endif
 
     @section('javascripts')
-        <script async src="/build-javascripts/app.js?v=1.4.4"></script>
+        <script async src="/build-javascripts/app.js?v=1.4.5"></script>
 
         @if(!Config::get('app.debug'))
         <script>
