f62779f718e6c148a186542d016e6da395980e87
Harmen <harmenstoppels@gmail.com>
2015-11-05 22:19:39 +0100

    Emoji font for Windows (fallback)

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -36,7 +36,7 @@
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=PT+Sans:400,700|PT+Serif:400,700,400italic">
     @section('stylesheets')
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.6.12">
+        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.7.0">
     @show
 </head>
 <body id="@yield('body-id', 'gsvnet')">
