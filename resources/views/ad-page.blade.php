238238af484f59c304c57a3184f29569cff94d08
Rick Rozemuller <rickrozemuller10@gmail.com>
2015-11-10 15:12:04 +0100

    Sponsorkliks added to sponsors page

--- resources/views/ad-page.blade.php
+++ resources/views/ad-page.blade.php
@@ -20,6 +20,16 @@
         </p>
 
         <p>U kunt de link ook doorsturen naar familie of bekenden <small>als zij toch al een aankoop gaan doen</small>! Kopieer hem dan hier: <input type="text" value="https://partnerprogramma.bol.com/click/click?p=1&t=url&s=33791&f=TXL&url=http%3A%2F%2Fwww.bol.com&name=GSV-tekstadvertentie" onclick="this.setSelectionRange(0, this.value.length);" /></p>
+
+        <h2>Sponsorkliks</h2>
+        <p>
+           Via Sponsorkliks kan de GSV een mooi bedrag meeverdienen wanneer u een aankoop doet bij een van de vele aangesloten winkels. Of u nu nieuwe kleding, elektronica of een pizza bestelt, zorg dat u deze link gebruikt! Voor aankopen bij Bol.com is het voor de GSV nog altijd voordeliger wanneer u de eerste link gebruikt.
+        </p>
+        <p>
+            <a href="http://www.sponsorkliks.com/winkels.php?club=5241" title="Sponsorkliks" class="button">Sponsorkliks</a>
+        </p>
+
+        <p>U kunt de link ook doorsturen naar familie of bekenden <small>als zij toch al een aankoop gaan doen</small>! Kopieer hem dan hier: <input type="text" value="https://partnerprogramma.bol.com/click/click?p=1&t=url&s=33791&f=TXL&url=http%3A%2F%2Fwww.bol.com&name=GSV-tekstadvertentie" onclick="this.setSelectionRange(0, this.value.length);" /></p>
     </article>
 
 @stop
