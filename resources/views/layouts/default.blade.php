2eff415aa13a0d5bbc2136e636d6d05fd7c45cce
Harmen <harmenstoppels@gmail.com>
2016-01-03 22:23:20 +0100

    Move analytics

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -90,15 +90,7 @@
         <script async src="/build-javascripts/app.js?v=1.4.11"></script>
 
         @if(!Config::get('app.debug'))
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-            ga('create', 'UA-48797155-1', {'cookieDomain': 'www.gsvnet.nl'});
-            ga('send', 'pageview');
-        </script>
+            @include('partials/_analytics')
         @endif
     @show
 </body>
