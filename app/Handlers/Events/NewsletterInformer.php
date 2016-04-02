61e69cd0912c8433e36bc1cb5339843c01cb6e80
Harmen <harmenstoppels@gmail.com>
2016-04-03 01:10:00 +0200

    Start logging

--- app/Handlers/Events/NewsletterInformer.php
+++ app/Handlers/Events/NewsletterInformer.php
@@ -4,6 +4,7 @@ use GSV\Events\Members\ProfileEvent;
 use GSVnet\Newsletters\NewsletterList;
 use GSVnet\Users\UserTransformer;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Log\Writer;
 
 class NewsletterInformer implements ShouldQueue
 {
@@ -16,16 +17,22 @@ class NewsletterInformer implements ShouldQueue
      * @var UserTransformer
      */
     private $transformer;
+    /**
+     * @var Writer
+     */
+    private $log;
 
     /**
      * NewsletterInformer constructor.
      * @param NewsletterList $newsletterList
      * @param UserTransformer $transformer
+     * @param Writer $log
      */
-    public function __construct(NewsletterList $newsletterList, UserTransformer $transformer)
+    public function __construct(NewsletterList $newsletterList, UserTransformer $transformer, Writer $log)
     {
         $this->list = $newsletterList;
         $this->transformer = $transformer;
+        $this->log = $log;
     }
 
     public function handle(ProfileEvent $event)
@@ -38,7 +45,7 @@ class NewsletterInformer implements ShouldQueue
             try {
                 $this->list->subscribeTo($user->type, $user->email, $data);
             } catch (\Mailchimp_Error $e) {
-                // simply ignore MailChimp errors for now.
+                $this->log->error('Something has gone wrong when accessing MailChimp: ' . $e->getMessage());
             }
         }
     }
