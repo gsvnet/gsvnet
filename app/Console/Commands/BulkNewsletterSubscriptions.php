e01afb6d612e0a28f410cdc5268ac11de4a9a0eb
Harmen <harmenstoppels@gmail.com>
2016-04-03 01:28:15 +0200

    Feature log even more
    Log

--- app/Console/Commands/BulkNewsletterSubscriptions.php
+++ app/Console/Commands/BulkNewsletterSubscriptions.php
@@ -6,21 +6,22 @@ use GSVnet\Users\User;
 use GSVnet\Users\UserTransformer;
 use GSVnet\Newsletters\NewsletterList;
 
-class BulkNewsletterSubscriptions extends Command {
+class BulkNewsletterSubscriptions extends Command
+{
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'newsletter:update-subscriptions';
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'newsletter:update-subscriptions';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Update newsletter subscriptions.';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update newsletter subscriptions.';
 
     protected $newsletterlist;
     protected $userTransformer;
@@ -31,23 +32,22 @@ class BulkNewsletterSubscriptions extends Command {
     ];
 
     public function __construct(NewsletterList $newsletterList, UserTransformer $userTransformer)
-	{
-		parent::__construct();
+    {
+        parent::__construct();
         $this->newsletterlist = $newsletterList;
         $this->userTransformer = $userTransformer;
-	}
+    }
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
         $for = $this->argument('for');
 
-        if(! array_key_exists($for, $this->values))
-        {
+        if (!array_key_exists($for, $this->values)) {
             $this->error('Kies uit leden of oud-leden');
             return;
         }
@@ -59,40 +59,33 @@ class BulkNewsletterSubscriptions extends Command {
         $unsubscribeBatch = $this->userTransformer->batchMailchimpUnsubscribe($users);
         $subscribeBatch = $this->userTransformer->batchMailchimpSubscribe($users);
 
-        // try {
-        //     $this->newsletterlist->batchUnsubscribeFrom($this->values['leden'], $unsubscribeBatch);
-        // } catch(\Mailchimp_Error $e) {}
-
-        // try {
-        //     $this->newsletterlist->batchUnsubscribeFrom($this->values['oud-leden'], $unsubscribeBatch);
-        // } catch(\Mailchimp_Error $e) {}
-
         try {
             $this->newsletterlist->batchSubscribeTo($type, $subscribeBatch);
-        } catch(\Mailchimp_Error $e) {}
+        } catch (\Mailchimp_Error $e) {
+			$this->info($e->getMessage());
+        }
+    }
 
-	}
-
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return [
-			['for', InputArgument::REQUIRED, 'Wie moeten er geupdate worden? leden|oud-leden '],
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['for', InputArgument::REQUIRED, 'Wie moeten er geupdate worden? leden|oud-leden '],
         ];
-	}
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
         return [];
-	}
+    }
 
 }
