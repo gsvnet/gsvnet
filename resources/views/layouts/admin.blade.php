dfeb467939ff338cf716f4d43252b866dc537b4c
Harmen <harmenstoppels@gmail.com>
2016-04-20 09:03:19 +0200

    New styles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,7 +13,7 @@
             <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
             <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
             <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.1.0/yeti/bootstrap.min.css">
-            <link rel="stylesheet" href="/stylesheets/admin.css?v=1.3.1">
+            <link rel="stylesheet" href="/stylesheets/admin.css?v=1.3.2">
         @show
 
         <link rel="shortcut icon" href="/favicon.png" />
