5f24853e1630886db3068539bcb3c0110ef265ad
Frits <fritsderoos@gmail.com>
2016-05-23 11:04:52 +0200

    added the new images in to the corresponding views

--- resources/views/de-gsv/pillars.blade.php
+++ resources/views/de-gsv/pillars.blade.php
@@ -21,8 +21,11 @@
             <ol class="carousel-indicators">
                 <li data-target="#christelijk-slideshow" data-slide-to="0" class="active"></li>
                 <li data-target="#christelijk-slideshow" data-slide-to="1"></li>
+                <li data-target="#christelijk-slideshow" data-slide-to="2"></li>
+                <li data-target="#christelijk-slideshow" data-slide-to="3"></li>
+                <li data-target="#christelijk-slideshow" data-slide-to="4"></li>
+                <li data-target="#christelijk-slideshow" data-slide-to="5"></li>
             </ol>
-
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
                 <div class="item active">
@@ -32,9 +35,27 @@
                     </div>
                 </div>
                 <div class="item">
-                    <img width="960" height="640" src="/images/over/christelijk-2.jpg" alt="Paasviering">
+                    <img width="960" height="640" src="/images/over/christelijk-2.jpg" alt="Diesconcert">
                     <div class="carousel-caption">
-                        Paasviering
+                       
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="960" height="640" src="/images/over/christelijk-3.jpg" alt="Bezinningsweekend">
+                    <div class="carousel-caption">
+                        Bezinningsweekend
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="960" height="640" src="/images/over/christelijk-6.jpg" alt="Bezinningsweekend">
+                    <div class="carousel-caption">
+                        Bezinningsweekend
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="960" height="640" src="/images/over/christelijk-7.jpg" alt="Bezinningsweekend">
+                    <div class="carousel-caption">
+                        Bezinningsweekend
                     </div>
                 </div>
             </div>
@@ -54,15 +75,35 @@
 
         <p>Omdat we een academische vereniging zijn is er op de GSV ook ruimte voor verdiepende activiteiten. Door het jaar heen zijn er lezingen over de meest uiteenlopende onderwerpen; bijvoorbeeld een (christelijk) wetenschappelijk thema of over iets uit het bedrijfsleven. Ook zijn er drie avonden per jaar waarop GSV’ers elkaar bijvoorbeeld wat bijbrengen over hun studie of waar ze wat vertellen over hun scriptie of buitenlandreis. Ook culturele filmavondjes, discussieavonden of een pubquiz behoren tot de activiteiten in de intellectuele pijler.</p>
 
-        <div class="carousel slide article-slideshow">
+        <div id="intellectueel-slideshow" class="carousel slide article-slideshow">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#intellectueel-slideshow" data-slide-to="0" class="active"></li>
+                <li data-target="#intellectueel-slideshow" data-slide-to="1"></li>
+            </ol>
+            <!-- Wrapper for slides -->
             <div class="carousel-inner">
                 <div class="item active">
-                    <img width="960" height="640" src="/images/over/intellectueel.jpg" alt="Lezing 'De Onlogische God' door J.A. Riemersma">
+                    <img width="960" height="640" src="/images/over/intellectueel-1.jpg" alt="Lezing 'De Onlogische God' door J.A. Riemersma">
                     <div class="carousel-caption">
                         Lezing 'De Onlogische God' door J.A. Riemersma
                     </div>
                 </div>
+                 <div class="item">
+                    <img width="960" height="640" src="/images/over/intellectueel-2.jpg" alt="Dispuut">
+                    <div class="carousel-caption">
+                        Dispuut
+                    </div>
+                </div>
             </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#intellectueel-slideshow" data-slide="prev">
+                <span class="i-back icon-prev"></span>
+            </a>
+            <a class="right carousel-control" href="#intellectueel-slideshow" data-slide="next">
+                <span class="i-forward icon-next"></span>
+            </a>
         </div>
 
         <p>Maar de GSV is meer dan alleen de activiteiten. Bij de GSV zul je een groep mensen treffen die als studenten elkaar motiveert om te studeren, of juist afspreekt om nu en dan even pauze te houden in de kantine van de Universiteitsbibliotheek. Kritische geluiden worden altijd gewaardeerd op de vereniging en voor een goed gesprek of een goede discussie zijn veel leden zeker wel warm te krijgen.</p>
@@ -77,6 +118,9 @@
                 <li data-target="#sociaal-slideshow" data-slide-to="0" class="active"></li>
                 <li data-target="#sociaal-slideshow" data-slide-to="1"></li>
                 <li data-target="#sociaal-slideshow" data-slide-to="2"></li>
+                <li data-target="#sociaal-slideshow" data-slide-to="3"></li>
+                <li data-target="#sociaal-slideshow" data-slide-to="4"></li>
+                                
             </ol>
 
             <!-- Wrapper for slides -->
@@ -88,15 +132,35 @@
                     </div>
                 </div>
                 <div class="item">
-                    <img width="960" height="640" src="/images/over/sociaal-2.jpg" alt="Themafeestje">
+                   <img width="960" height="640" src="/images/over/sociaal-2.jpg" alt="Sociëteit &mdash; achterzaal">
                     <div class="carousel-caption">
-                        Themafeestje
+                        Sociëteit &mdash; achterzaal
+                    </div>
+                </div>
+                <!--
+                <div class="item">
+                   <img width="960" height="640" src="/images/over/sociaal-3.jpg" alt="Spelletjesavond">
+                    <div class="carousel-caption">
+                        Spelletjesavond
+                    </div>
+                </div>
+                -->
+                <div class="item">
+                   <img width="960" height="640" src="/images/over/sociaal-4.jpg" alt="Sociëteit &mdash; voorzaal">
+                    <div class="carousel-caption">
+                        Sociëteit &mdash; voorzaal
                     </div>
                 </div>
                 <div class="item">
-                    <img width="960" height="640" src="/images/over/sociaal-3.jpg" alt="Spelletjesavond">
+                   <img width="960" height="640" src="/images/over/sociaal-5.jpg" alt="Sociëteit &mdash; achterzaal">
                     <div class="carousel-caption">
-                        Spelletjesavond
+                        Sociëteit &mdash; achterzaal
+                    </div>
+                </div>
+                 <div class="item">
+                   <img width="960" height="640" src="/images/over/sociaal-6.jpg" alt="Sociëteit &mdash; achterzaal">
+                    <div class="carousel-caption">
+                        Sociëteit &mdash; achterzaal
                     </div>
                 </div>
             </div>
@@ -122,26 +186,40 @@
                 <li data-target="#studentikoos-slideshow" data-slide-to="0" class="active"></li>
                 <li data-target="#studentikoos-slideshow" data-slide-to="1"></li>
                 <li data-target="#studentikoos-slideshow" data-slide-to="2"></li>
+                <li data-target="#studentikoos-slideshow" data-slide-to="3"></li>
+                <li data-target="#studentikoos-slideshow" data-slide-to="4"></li>
             </ol>
 
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
                 <div class="item active">
-                    <img width="960" height="640" src="/images/over/studentikoos-1.jpg" alt="Huishoudelijke vergadering">
+                    <img width="960" height="640" src="/images/over/studentikoos-1.jpg" alt="Dies Natalis">
                     <div class="carousel-caption">
-                        Huishoudelijke vergadering
+                        Dies Natalis
                     </div>
                 </div>
-                <div class="item">
-                    <img width="960" height="640" src="/images/over/studentikoos-2.jpg" alt="Dies Natalis">
+                    <div class="item">
+                    <img width="960" height="640" src="/images/over/studentikoos-4.jpg" alt="Dies Natalis">
                     <div class="carousel-caption">
                         Dies Natalis
                     </div>
                 </div>
                 <div class="item">
-                    <img width="960" height="640" src="/images/over/studentikoos-3.jpg" alt="Dies Natalis: jaarverbandsfoto">
+                    <img width="960" height="640" src="/images/over/studentikoos-2.jpg" alt="Dies Natalis: jaarverbandsfoto">
                     <div class="carousel-caption">
                         Dies Natalis: jaarverbandsfoto
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="960" height="640" src="/images/over/studentikoos-3.jpg" alt="Academiegebouw: jaarverbandsfoto">
+                    <div class="carousel-caption">
+                        Academiegebouw: jaarverbandsfoto
+                    </div>
+                </div>
+                <div class="item">
+                    <img width="960" height="640" src="/images/over/studentikoos-5.jpg" alt="Dies Natalis">
+                    <div class="carousel-caption">
+                        Dies Natalis
                     </div>
                 </div>
             </div>
