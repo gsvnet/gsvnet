138bf8c05eb5cfb199d92a23277d64066e89e61e
Harmen Stoppels <harmenstoppels@gmail.com>
2016-09-13 03:14:56 +0200

    fix some more addresses

--- app/Console/Commands/StandardizeAddresses.php
+++ app/Console/Commands/StandardizeAddresses.php
@@ -100,10 +100,10 @@ class StandardizeAddresses extends Command
             );
 
             $new = new Address(
-                preg_replace('/\s+/', ' ', ucfirst(mb_strtolower(trim($user->profile->address)))),
+                preg_replace('/\s+/', ' ', ucwords(mb_strtolower(trim($user->profile->address)))),
                 preg_replace('/^(\d{4})\s*([A-Z]{2})$/', '$1$2', trim(mb_strtoupper($user->profile->zip_code))),
-                preg_replace('/\s+/', ' ', ucfirst(mb_strtolower(trim($user->profile->town)))),
-                preg_replace('/\s+/', ' ', ucfirst(mb_strtolower(trim($user->profile->country)))) ?: 'Nederland'
+                preg_replace('/\s+/', ' ', ucwords(mb_strtolower(trim($user->profile->town)))),
+                preg_replace('/\s+/', ' ', ucwords(mb_strtolower(trim($user->profile->country)))) ?: 'Nederland'
             );
 
             if ($old->equals($new)) {
