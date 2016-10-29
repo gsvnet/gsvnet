04907fb6cde96d4a2f5e5c61b33dda29fb7c9ec0
Harmen <harmenstoppels@gmail.com>
2016-10-29 13:52:47 +0200

    Oudledenpagina

--- resources/views/de-gsv/former-members.blade.php
+++ resources/views/de-gsv/former-members.blade.php
@@ -11,14 +11,28 @@
             <p class="lead">De GSV heeft meer dan vijftienhonderd oud-leden. Jaarlijks wordt er een oud-ledenborrel georganiseerd.</p>
         </div>
 
-        <h2>Oud-ledenborrel 29 april 2016</h2>
-        <p>De jaarlijkse oudledenborrel voor reünisten van de GSV vindt dit jaar plaats op vrijdag 29 april. Vroeger was dit natuurlijk de nacht voor Koninginnedag, we brengen deze avond eenmalig terug voor onze oud-leden! We beginnen de avond met een pubquiz om 20:00. De Malversacie zal zorgen voor hapjes, foto's uit de oude doos, een geupdate stamboom en ander vermaak.
-        <p>Nodig vooral leden uit uw jaarverband uit! Hoe meer zielen, hoe meer vreugd.</p>
-        <p>Als je Facebook hebt, meld je dan aan op de <a href="https://www.facebook.com/events/587628854726340/">evenementpagina!</a> </p>
-        <h2>Vragen?</h2>
-        <p>Voor meer informatie en vragen kunt u mailen naar {!!HTML::mailto('malversacie@gsvnet.nl')!!}.</p>
+        <h2>Lustrum 2016</h2>
+        <h3>Musical 21 november</h3>
+
         <figure>
-            <img src="/images/oudleden.jpg" width="960" alt="Oud-ledenborrel 29 april 2016 bij Hereweg 40">
+            <img src="/images/musical.jpg" width="960" alt="Musical 21 november 2016">
         </figure>
+
+        <p>Kaartjes kunnen gereserveerd worden <a href="http://www.lustrumintense.nl/musical/">op de website</a>, maar kunnen ook aan de deur gekocht worden.</p>
+
+        <h3>Oud-ledenborrel Dies 25 november</h3>
+
+        <figure>
+            <img src="/images/diesborrel.jpg" width="960" alt="Oud-ledenborrel 25 november 2016">
+        </figure>
+
+        <p>Opgave voor de borrel kan via <a href="http://www.lustrumintense.nl/dies.htm">http://www.lustrumintense.nl/dies.htm</a></p>
+
+        <h3>Lustrumdies 25 november</h3>
+        <p>U kunt zich vanaf heden opgeven voor de Dies Natalis die op vrijdag 25 november plaatsvindt. Als oud-lid bent u welkom vanaf 18:00, dan begint het amicitiadrinken. Aansluitend zal een heerlijk diner verzorgd worden, inclusief speeches en ander vermaak. Het feest wordt dit jaar in de stad Groningen gehouden, om precies te zijn in <a href="http://www.paradigm050.com/nl/">Paradigm</a>. Dit pand kunt u vinden aan de Helsinkilaan 6, vlak bij de Europaweg. Komt u nou bij dat pand aan en u denkt: 'Help, wat is dit? Zit ik wel goed?', stapt u dan vooral naar binnen, industrieel is hip.</p>
+
+        <p>Meld u snel aan! Er is namelijk een beperkt aantal plekken vrij, er is capaciteit voor 250 mensen. Vol = vol! Voor oud-leden is het verder niet verplicht om met een date te komen, u kunt zich gewoon aanmelden.</p>
+        <p><a href="http://www.lustrumintense.nl/dies.htm" class="button">Aanmelden</a></p>
+        <p>Voor meer informatie en vragen kunt u mailen naar {!!HTML::mailto('malversacie@gsvnet.nl')!!}.</p>
     </article>
 @stop
