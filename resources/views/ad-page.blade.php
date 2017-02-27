c3cb40bd4407de51e6b72db85adc6da1db2ad2f7
Rick Rozemuller <rickrozemuller10@gmail.com>
2017-02-27 22:46:44 +0100

    Updated sponsorpage

--- resources/views/ad-page.blade.php
+++ resources/views/ad-page.blade.php
@@ -19,8 +19,6 @@
             <a href="https://partnerprogramma.bol.com/click/click?p=1&t=url&s=33791&f=TXL&url=http%3A%2F%2Fwww.bol.com&name=GSV-tekstadvertentie" title="Bol.com" class="button">Bol.com</a>
         </p>
 
-        <p>U kunt de link ook doorsturen naar familie of bekenden <small>als zij toch al een aankoop gaan doen</small>! Kopieer hem dan hier: <input type="text" value="https://partnerprogramma.bol.com/click/click?p=1&t=url&s=33791&f=TXL&url=http%3A%2F%2Fwww.bol.com&name=GSV-tekstadvertentie" onclick="this.setSelectionRange(0, this.value.length);" /></p>
-
         <h2>Sponsorkliks</h2>
         <p>
            Via Sponsorkliks kan de GSV een mooi bedrag meeverdienen wanneer u een aankoop doet bij een van de vele aangesloten winkels. Of u nu nieuwe kleding, elektronica of een pizza bestelt, zorg dat u deze link gebruikt! Voor aankopen bij Bol.com is het voor de GSV nog altijd voordeliger wanneer u de eerste link gebruikt.
@@ -29,7 +27,38 @@
             <a href="http://www.sponsorkliks.com/winkels.php?club=5241" title="Sponsorkliks" class="button">Sponsorkliks</a>
         </p>
 
-        <p>U kunt de link ook doorsturen naar familie of bekenden <small>als zij toch al een aankoop gaan doen</small>! Kopieer hem dan hier: <input type="text" value="http://www.sponsorkliks.com/winkels.php?club=5241" onclick="this.setSelectionRange(0, this.value.length);" /></p>
+        <h2>Rijschool Nova</h2>
+        <p>
+            De eerste deal is bij velen van jullie waarschijnlijk wel bekend: namelijk de deal met Rijschool Nova.
+            Naast een slagingspercentage van 75% (aldus de site van Nova) letten zij ook nog eens extra op het milieu en proberen ze ook zo milieuvriendelijk mogelijk te rijden en te leren rijden.
+            Deze deal is vrij simpel: de GSV krijgt €50,- voor elke persoon die lest bij deze rijschool. Verder krijgt de desbetreffende persoon zelf ook nog eens €50,- korting bovenop het geld wat naar de GSV gaat.
+            Een pakket van 30 lessen van 60 minuten, met tussentijdse toets, kost nu €1457 i.p.v. €1507! Als u lessen volgt/gaat volgen bij rijschool Nova geef dit dan even door aan iemand van de commercie zodat de Fiscus een factuur kan sturen.
+        </p>
+
+        <h2>PerspectieF Groningen</h2>
+        <p>
+            Sinds kort hebben we ook een deal met PerspectieF Groningen. PerspectieF is de jongerenorganisatie van de ChristenUnie. Door middel van politieke acties en bijeenkomsten proberen zij ervoor te zorgen dat de jongeren meer invloed kunnen uitoefenen op de politiek en ermee kunnen kennismaken en betrokken worden in de politiek. Voor meer informatie verwijzen wij u door naar amice Haveman (jongerenambassadeur voor PerspectieF Groningen). De deal die wij hebben ziet er als volgt uit: voor elke GSV’er die lid wordt van PerspectieF Groningen krijgt de GSV €10,-. Dit is nog niet alles, de GSV’er die lid is geworden krijgt namelijk ook nog eens €10,- korting op lidmaatschap of evenementen van PerspectieF! Het lidmaatschap voor het eerste jaar kost met de korting €5,- (i.p.v. €12,50). De overige korting (€2,50) krijgt u op een andere manier terug. Ook voor deze deal geldt: als je lid wordt geef dit door aan iemand van de commercie zodat de Fiscus een factuur kan sturen.
+        </p>
+
+        <h2>Gents</h2>
+        <p>
+            Bij Gents kunt u naast de jaarlijkse rokkostuumsactie, die vooral altijd goed benut wordt door de sjaars, ook nog eens 10% korting krijgen. Vermeld bij uw aankoop dat u lid bent van de GSV en uw korting is binnen!
+        </p>
+
+        <h2>Grofietsen</h2>
+        <p>
+            Inmiddels kent iedereen hem wel door de GSV-app, maar voor de volledigheid zal dit contract hier ook nog even kort vernoemd worden. Als je bij Grofietsen (Korreweg 22) je fiets aanschaft en je vermeldt dat je lid bent van de GSV, dan krijg je 20% korting. Geef daarnaast ook even bij iemand van de commercie aan dat je daar een fiets gekocht hebt, want bovenop deze korting krijgen we ook nog eens een nader te bepalen bedrag cadeau.
+        </p>
+
+        <h2>CopyCopy</h2>
+        <p>
+            Voor al uw print-, scan- en bindwerk van werkstukken en posters kunt u terecht bij CopyCopy in de Oude Kijk in Het Jatstraat 52. Bij vermelding dat je van de GSV bent krijg je hier 10% korting op het totaalbedrag.
+        </p>
+
+        <h2>Patrimonium</h2>
+        <p>
+            Ook Patrimonium zorgt voor geld op de GSV-rekening. Wij krijgen namelijk €100 als zij een huis aanbieden en wij deze kunnen vullen met GSV’ers, of dit namens een GSV’er kunnen vullen. Hier kunnen we niet veel aan doen totdat ons een huis wordt aangeboden.
+        </p>
     </article>
 
 @stop
