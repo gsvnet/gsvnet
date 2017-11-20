c552e55b7be392d1b15475e835b07a79cf79c171
Rick Rozemuller <rickrozemuller10@gmail.com>
2017-11-20 23:35:00 +0100

    Meer advertenties

--- resources/views/ad-page.blade.php
+++ resources/views/ad-page.blade.php
@@ -27,17 +27,13 @@
             <a href="http://www.sponsorkliks.com/winkels.php?club=5241" title="Sponsorkliks" class="button">Sponsorkliks</a>
         </p>
 
-        <h2>Rijschool Nova</h2>
+        <h2>Rijschool Koster</h2>
         <p>
-            De eerste deal is bij velen van jullie waarschijnlijk wel bekend: namelijk de deal met Rijschool Nova.
-            Naast een slagingspercentage van 75% (aldus de site van Nova) letten zij ook nog eens extra op het milieu en proberen ze ook zo milieuvriendelijk mogelijk te rijden en te leren rijden.
-            Deze deal is vrij simpel: de GSV krijgt €50,- voor elke persoon die lest bij deze rijschool. Verder krijgt de desbetreffende persoon zelf ook nog eens €50,- korting bovenop het geld wat naar de GSV gaat.
-            Een pakket van 30 lessen van 60 minuten, met tussentijdse toets, kost nu €1457 i.p.v. €1507! Als u lessen volgt/gaat volgen bij rijschool Nova geef dit dan even door aan iemand van de commercie zodat de Fiscus een factuur kan sturen.
-        </p>
-
-        <h2>PerspectieF Groningen</h2>
-        <p>
-            Sinds kort hebben we ook een deal met PerspectieF Groningen. PerspectieF is de jongerenorganisatie van de ChristenUnie. Door middel van politieke acties en bijeenkomsten proberen zij ervoor te zorgen dat de jongeren meer invloed kunnen uitoefenen op de politiek en ermee kunnen kennismaken en betrokken worden in de politiek. Voor meer informatie verwijzen wij u door naar amice Haveman (jongerenambassadeur voor PerspectieF Groningen). De deal die wij hebben ziet er als volgt uit: voor elke GSV’er die lid wordt van PerspectieF Groningen krijgt de GSV €10,-. Dit is nog niet alles, de GSV’er die lid is geworden krijgt namelijk ook nog eens €10,- korting op lidmaatschap of evenementen van PerspectieF! Het lidmaatschap voor het eerste jaar kost met de korting €5,- (i.p.v. €12,50). De overige korting (€2,50) krijgt u op een andere manier terug. Ook voor deze deal geldt: als je lid wordt geef dit door aan iemand van de commercie zodat de Fiscus een factuur kan sturen.
+            Rijschool Koster, al jaren het adres voor een goede en betrouwbare rijopleiding.
+            Goed vanwege de resultaten, betrouwbaar mede vanwege Bovag lidmaatschap en Bovag garantie.
+            Omdat niet iedereen hetzelfde is, zijn er verschillende mogelijkheden.
+            Op de website, <a href="http://www.rijschoolkoster.nl">www.rijschoolkoster.nl</a>, kun je meer informatie vinden over de rijopleiding die bij jou past.
+            Misschien tot ziens, Bert Koster
         </p>
 
         <h2>Gents</h2>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -165,10 +165,13 @@
                         @endif
                     </ul>
                     <h2>Sponsors</h2>
-                    <p><img src="/images/ads/patrimonium.gif" title="Patrimonium" /></p>
-                    <p><a href="http://www.oudmankeukens.nl/" title="Oudman Keukens" rel="nofollow"><img src="/images/ads/oudman.png" title="Oudman keukens" /></a></p>
-                    <p><a href="https://www.hoyhoy.nl/energie-vergelijken/" title="energievergelijker"><img src="/images/ads/hoyhoy.gif" title="energievergelijker" alt="energievergelijker" /></a></p>
-                    <p><a href="https://www.pricewise.nl/energie-vergelijken/" title="energie vergelijken"><img src="/images/ads/pricewise.gif" title="energie vergelijken" alt="energie vergelijken" /></a></p>
+                    <p><img src="/images/annonces/patrimonium.gif" title="Patrimonium" /></p>
+                    <p><a href="http://www.oudmankeukens.nl/" title="Oudman Keukens" rel="nofollow"><img src="/images/annonces/oudman.png" title="Oudman keukens" /></a></p>
+                    <p><a href="https://www.hoyhoy.nl/energie-vergelijken/" title="energievergelijker"><img src="/images/annonces/hoyhoy.gif" title="energievergelijker" alt="energievergelijker" /></a></p>
+                    <p><a href="https://www.pricewise.nl/energie-vergelijken/" title="energie vergelijken"><img src="/images/annonces/pricewise.gif" title="energie vergelijken" alt="energie vergelijken" /></a></p>
+                    <p><a href="https://www.autoverzekering.nl/" title="Autoverzekering vergelijken"><img src="/images/annonces/autoverzekering_nl.png" title="Autoverzekering vergelijken" alt="Autoverzekering vergelijken" /></a></p>
+                    <p><a href="https://www.overstappen.nl/autoverzekering/" title="Autoverzekering"><img src="/images/annonces/overstappen_nl.png" title="Autoverzekering" alt="Autoverzekering" /></a></p>
+                    <p><a href="http://www.rijschoolkoster.nl/" title="Rijschool Koster"><img src="/images/annonces/rijschoolkoster.png" title="Rijschool Koster" alt="Rijschool Koster" /></a></p>
                 </div>
             </div>
         </div>
