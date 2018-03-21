460965e733f4d482b82419b1396079cd90bbb320
Rick Rozemuller <rickrozemuller10@gmail.com>
2018-03-21 23:07:59 +0100

    Force cache update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -38,7 +38,7 @@
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=PT+Sans:400,700|PT+Serif:400,700,400italic">
     @section('stylesheets')
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.7.9">
+        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.8.0">
     @show
 </head>
 <body id="@yield('body-id', 'gsvnet')">
@@ -132,7 +132,7 @@
         </script>
         @endif
 
-        <script async src="/build-javascripts/app.js?v=1.5.5"></script>
+        <script async src="/build-javascripts/app.js?v=1.5.6"></script>
 
         @if(!Config::get('app.debug'))
             @include('partials/_analytics')
