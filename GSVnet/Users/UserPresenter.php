ef21b2d0f402829c0821665d74e5f184af7e6f65
Rick Rozemuller <rickrozemuller10@gmail.com>
2018-11-03 13:59:40 +0100

    Revert "Stop using random avatars"
    This reverts commit 43202be1b3b07aa3ce6c6bc909ea2619613f5b02.

--- GSVnet/Users/UserPresenter.php
+++ GSVnet/Users/UserPresenter.php
@@ -103,6 +103,11 @@ class UserPresenter extends Presenter
 
     public function getAvatar($size = 120, $className = '')
     {
+      if ($this->avatar >= 100)
+        return "https://randomuser.me/api/portraits/women/".($this->avatar - 100).".jpg";
+      elseif ($this->avatar >= 0)
+        return "https://randomuser.me/api/portraits/men/".$this->avatar.".jpg";
+
         return Gravatar::image($this->email, $size, 'mm', null, null, true);
         //return HTML::image($url, 'Avatar', ['width' => $size, 'height' => $size]);
     }
@@ -110,7 +115,12 @@ class UserPresenter extends Presenter
     // Let op: deze functie returned HTML, de functie hierboven returned een URL
     public function getAvatarDeferred($size = 120)
     {
-        $url = Gravatar::image($this->email, $size, 'mm', null, null, true);
+        if ($this->avatar >= 100)
+          $url = "https://randomuser.me/api/portraits/women/".($this->avatar - 100).".jpg";
+        elseif ($this->avatar >= 0)
+          $url = "https://randomuser.me/api/portraits/men/".$this->avatar.".jpg";
+        else
+          $url = Gravatar::image($this->email, $size, 'mm', null, null, true);
 
         return '<span class="img-wrap" data-gravatar-url="' . $url . '" data-gravatar-size="' . $size . '"></span>';
     }
