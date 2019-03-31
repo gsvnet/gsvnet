3a58076487b50b8dbcb719e76b9e3f8db2551b85
Rick Rozemuller <rickrozemuller10@gmail.com>
2019-04-01 00:22:38 +0200

    bump version numbers

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -38,7 +38,7 @@
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=PT+Sans:400,700|PT+Serif:400,700,400italic">
     @section('stylesheets')
         <!-- Stylesheets -->
-        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.8.5">
+        <link rel="stylesheet" href="/stylesheets/screen.css?v=1.9.0">
     @show
 
     <?php
@@ -159,7 +159,7 @@
         </script>
         @endif
 
-        <script async src="/build-javascripts/app.js?v=1.5.7"></script>
+        <script async src="/build-javascripts/app.js?v=1.6.0"></script>
 
         @if(!Config::get('app.debug'))
             @include('partials/_analytics')
