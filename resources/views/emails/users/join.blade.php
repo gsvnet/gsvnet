972ce3710154f71035a046ff20d1035547cd4acd
Valkje <loran.knol@gmail.com>
2019-05-13 19:33:48 +0200

    Fixed spelling.

--- resources/views/emails/users/join.blade.php
+++ resources/views/emails/users/join.blade.php
@@ -7,6 +7,6 @@
 @section('content')
     <h2>Beste {{ $fullname }}</h2>
     <p>Je hebt zojuist een fantastische keuze in je leven gemaakt door je officieel aan te melden bij de GSV!</p>
-    <p>De Prescie zal zo spoedig mogelijk contact met je opnemen en je meer informatie geven over het novitiaat.</p>
+    <p>De PRescie zal zo spoedig mogelijk contact met je opnemen en je meer informatie geven over het novitiaat.</p>
     <p>Vriendelijke groeten namens de webcie!</p>
 @stop
