9311fd3f555957b5c4b12138395b8dc8cb4765d0
Rick Rozemuller <rickrozemuller10@gmail.com>
2019-08-05 22:36:01 +0200

    Adhere to Bugsnag's current setup instructions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,9 @@ class AppServiceProvider extends ServiceProvider {
 			'Illuminate\Contracts\Auth\Registrar',
 			'GSV\Services\Registrar'
 		);
+
+		$this->app->alias('bugsnag.logger', \Illuminate\Contracts\Logging\Log::class);
+		$this->app->alias('bugsnag.logger', \Psr\Log\LoggerInterface::class);
 	}
 
 }
