ba206dc677cbb4237b244e5be248ad90826e3255
Rick Rozemuller <rickrozemuller10@gmail.com>
2019-08-31 17:08:56 +0200

    Fixed incorrect display of membership status on stats page

--- resources/views/forum/_posts_row.blade.php
+++ resources/views/forum/_posts_row.blade.php
@@ -1,4 +1,4 @@
-<tr class="{{ $user->isReunist() ? 'stats--former-member' : '' }}">
+<tr class="{{ !$user->isMember() ? 'stats--former-member' : '' }}">
 	<td>{{ $index }}</td>
 	<td><a href="{{URL::action('UserController@showUser', ['id' => $user->id])}}">{{$user->present()->fullname}}</a></td>
 	<td>{{ $user->num }}</td>
