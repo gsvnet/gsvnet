9eb73082d5c40c6cf49b0ada81d40e007c2d774b
Rick Rozemuller <rickrozemuller10@gmail.com>
2020-04-01 19:44:09 +0200

    Balance patch

--- resources/assets/front/javascripts/april_fools/ant/ant.js
+++ resources/assets/front/javascripts/april_fools/ant/ant.js
@@ -4,7 +4,11 @@ import Util from './components/Util'
 
 function startAnt() {
     const scene = new Scene()
-    const ant = new Ant(['p', 'h1', 'h2', 'h3', '.form-group', '.avatar', '.author'])
+    const ant = new Ant([
+        'p', 'h1', 'h2', 'h3', '.form-group', '.avatar', '.like-box', '.media-counter',
+        '.author', '.tags', '.list-title', '.list-description', '.media-details > a > span',
+        '.media-body .inline-list', '.pagination > li', '.photo-tile', '.profile-image'
+    ])
     window.ant = ant
     console.log(ant)
     scene.add(ant)
