9150b357f572be51596944fd7fb3675af603e7d1
Valkje <loran.knol@gmail.com>
2020-04-22 12:25:23 +0200

    Revert "Merge pull request #272 from haampie/aprilfools20"
    This reverts commit 4adb55a2b430c6e114aa4b9da546316dc55dce58, reversing
    changes made to 37b32a69fef0470d5ed82405ad00e5c7919dcf96.

--- resources/assets/front/javascripts/april_fools/ant/ant.js
+++ resources/assets/front/javascripts/april_fools/ant/ant.js
@@ -4,11 +4,7 @@ import Util from './components/Util'
 
 function startAnt() {
     const scene = new Scene()
-    const ant = new Ant([
-        'p', 'h1', 'h2', 'h3', '.form-group', '.avatar', '.like-box', '.media-counter',
-        '.author', '.tags', '.list-title', '.list-description', '.media-details > a > span',
-        '.media-body .inline-list', '.pagination > li', '.photo-tile', '.profile-image'
-    ])
+    const ant = new Ant(['p', 'h1', 'h2', 'h3', '.form-group', '.avatar', '.author'])
     window.ant = ant
     console.log(ant)
     scene.add(ant)
