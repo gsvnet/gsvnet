86a8ef946a46301520098b181a3b2c906c8edc4e
Valkje <loran.knol@gmail.com>
2020-04-22 12:05:11 +0200

    Remove ads

--- resources/views/partials/_directads.blade.php
+++ resources/views/partials/_directads.blade.php
@@ -3,15 +3,9 @@
     <div class="annoncesholder">
         <p><a href="https://smartranking.nl/"><img src="/images/annonces/smartranking-logo.jpg" title="SmartRanking" /></a></p>
         <p><a href="http://www.oudmankeukens.nl/" title="Oudman Keukens" rel="nofollow"><img src="/images/annonces/oudman.png" title="Oudman keukens" /></a></p>
-        <p><a href="https://www.hoyhoy.nl/energie-vergelijken/" title="energievergelijker"><img src="/images/annonces/hoyhoy.gif" title="energievergelijker" alt="energievergelijker" /></a></p>
-        <p><a href="https://www.pricewise.nl/energie-vergelijken/" title="energie vergelijken"><img src="/images/annonces/pricewise.gif" title="energie vergelijken" alt="energie vergelijken" /></a></p>
-        <p><a href="https://www.autoverzekering.nl/" title="Autoverzekering vergelijken"><img src="/images/annonces/autoverzekering_nl.png" title="Autoverzekering vergelijken" alt="Autoverzekering vergelijken" /></a></p>
-        <p><a href="https://www.overstappen.nl/autoverzekering/" title="Autoverzekering"><img src="/images/annonces/overstappen_nl.png" title="Autoverzekering" alt="Autoverzekering" /></a></p>
         <p><a href="http://www.rijschoolkoster.nl/" title="Rijschool Koster"><img src="/images/annonces/rijschoolkoster.png" title="Rijschool Koster" alt="Rijschool Koster" /></a></p>
         <p><a href="http://www.dressmeclothing.nl/" title="Kledingdrukkerij DressMe Clothing"><img src="/images/annonces/dressmeclothing.png" title="Kledingdrukkerij DressMe Clothing" alt="Kledingdrukkerij DressMe Clothing" /></a></p>
-        <p><a href="https://www.uitvaartverzekeringvergelijken.com" title="Uitvaartverzekeringen vergelijken"><img src="/images/annonces/uitvaartverzekering.png" title="Uitvaartverzekeringen vergelijken" alt="uitvaartverzekeringvergelijken.com" /></a></p>
         <p><a href="https://www.werkenbijbelsimpel.nl" title="Werkenbijbelsimpel.nl"><img src="/images/annonces/logo_belsimpel.png" title="Werkenbijbelsimpel.nl" alt="Werkenbijbelsimpel.nl" /></a></p>
-        <p><a href="https://www.easyswitch.nl/energie-vergelijken/" title="energie vergelijken"><img src="/images/annonces/easyswitch.png" title="energie vergelijken" alt="energie vergelijken" /></a></p>
         <p><a href="https://www.inshared.nl/autoverzekering" title="autoverzekering"><img src="/images/annonces/inshared.png" title="autoverzekering" alt="autoverzekering" /></a></p>
         <p><a href="https://skepp.nl/nl/" title="Skepp"><img src="/images/annonces/Logo SKEPP.jpg" title="Skepp" alt="Skepp" /></a></p>
         <p><a href="https://www.geld.nl/" title="Geld.nl"><img src="/images/annonces/geld_nl.png" title="Geld.nl" alt="Geld.nl" /></a></p>
