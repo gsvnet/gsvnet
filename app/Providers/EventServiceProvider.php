23c8a886619c863a6adbfaef1226cfa54688a440
lasschehj <lassche.hj@gmail.com>
2023-02-07 10:38:23 +0100

    Core EventServiceProvider overwrite

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,29 +2,31 @@
 
 namespace App\Providers;
 
-use App\Handlers\Events\ThreadEventHandler;
-use App\Handlers\Events\UserEventHandler;
-use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
 {
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
     protected $listen = [
-    ];
-
-    protected $subscribe = [
-        ThreadEventHandler::class,
-        UserEventHandler::class,
+        'App\Events\SomeEvent' => [
+            'App\Listeners\EventListener',
+        ],
     ];
 
     /**
-     * Register any other events for your application.
+     * Register any events for your application.
      *
-     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
      * @return void
      */
-    public function boot(DispatcherContract $events)
+    public function boot()
     {
-        parent::boot($events);
+        parent::boot();
+
+        //
     }
 }
