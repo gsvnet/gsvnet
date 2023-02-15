4dad68036d9e1a8b1850af0f1eb98cc9766cec9d
Shift <shift@laravelshift.com>
2023-02-15 14:24:09 +0000

    Shift middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'csrf' => \App\Http\Middleware\VerifyCsrfToken::class,
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth' => \App\Http\Middleware\Authenticate::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'tokenAuth' => \App\Http\Middleware\ApiAuthenticated::class,
@@ -52,5 +52,20 @@ class Kernel extends HttpKernel
         'checkDate' => \App\Http\Middleware\ValidEventDate::class,
         'notYetMember' => \App\Http\Middleware\CanBecomeMember::class,
         'loginViaToken' => \App\Http\Middleware\LoginViaToken::class,
+    ];
+    /**
+     * The priority-sorted list of middleware.
+     *
+     * This forces non-global middleware to always be in the given order.
+     *
+     * @var array
+     */
+    protected $middlewarePriority = [
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\Authenticate::class,
+        \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        \Illuminate\Auth\Middleware\Authorize::class,
     ];
 }
