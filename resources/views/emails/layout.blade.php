f579f5b13bef8769670afc2389996940817da02e
Shift <shift@laravelshift.com>
2023-02-15 14:24:09 +0000

    Convert Blade `or` to PHP `??`

--- resources/views/emails/layout.blade.php
+++ resources/views/emails/layout.blade.php
@@ -25,7 +25,7 @@ Email on Acid - http://www.emailonacid.com/blog/details/C18/doctype_-_the_black_
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<title>{{{$title or 'GSVnet'}}}</title>
+	<title>{{{$title ?? 'GSVnet'}}}</title>
 	<style type="text/css">
 
 		/***********
